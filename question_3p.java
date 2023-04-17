6d442e559f062a40e554c5db1778da8ac6072b6c
Md Taofick <107477685+taofick221@users.noreply.github.com>
2023-04-17 21:29:25 +0600

    Add files via upload

---
+++ question_3p.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class question_3p {
+    public static void main (String[] args){
+//         Question 1
+//        Write a program to sum three numbers in java
+//    float a=10;
+//    float b=10;
+//    float c =2;
+//    float sum = (a+b+c)/3;
+//        System.out.println(sum);
+//        Question 2
+//        Write a program to calculate CGPA using marks of three subject (out of 100)
+//        Scanner input= new Scanner(System.in);
+//        System.out.println("Enter your math mark :");
+//        float math= input.nextFloat();
+//        System.out.println("Enter your english mark :");
+//        float English= input.nextFloat();
+//        System.out.println("Enter your java mark :");
+//        float java= input.nextFloat();
+//        float CGPA = (math+English+java)/3;
+//        System.out.println(CGPA);
+
+
+//        Question 3
+//        Write a program to which asks the user to
+//        enter his name and print them with "hello <name> have a good day" text
+//         Scanner input= new Scanner(System.in);
+//        System.out.println("Enter your name and take good message :");
+//        String name= input.nextLine();
+//        System.out.println("Hello " + name + " have a good day");
+
+
+//        Question 4
+//    Write a java program to detect whether a number input from
+//    the user is integer or not
+//      System.out.println("Enter a number :");
+//        Scanner input= new Scanner(System.in);
+//        boolean number= input.hasNextInt();
+//        System.out.println(number);
+
+    }
+
+
+}
---
+++ user_input_2p.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class user_input_2p {
+    public static void main(String[] args) {
+
+        System.out.println("Taking input from the user");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number 1");
+//
+//        int a = sc.nextInt();
+//
+//        float a = sc.nextFloat();
+//        System.out.println("Enter number 2");
+//        float b= sc.nextFloat();
+//
+//        int a = sc.nextInt();
+//        Float sum=a+b;
+//
+//        System.out.println("The sum of these number is");
+//        System.out.println(sum);
+//
+//
+//        boolean b1= sc.hasNextInt();
+//        System.out.println(b1);
+//
+//
+//      String ch= sc.next();
+        String ch= sc.nextLine();
+        System.out.println(ch);
+    }
+}
