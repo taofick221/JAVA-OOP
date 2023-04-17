fcbb4a3d064d699491664c8771bc5003cbe71e1d
Md Taofick <107477685+taofick221@users.noreply.github.com>
2023-04-17 21:28:31 +0600

    Add files via upload

---
+++ Literals_Java_1p.java
@@ -0,0 +1,15 @@
+public class Literals_Java_1p {
+public static void main(String[] args){
+    byte age1= 24;
+    int age2= 6547;
+    short age3= 8444;
+    long age4= 444444444444444l;
+    float age5= 5.6f;
+    double age6= 4.66;
+    boolean age7= true;
+    char age8= 'A';
+    System.out.println(age1);
+    String str= "Taofick";
+    System.out.println(str);
+}
+}
