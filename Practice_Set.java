d9e66ba2560924189cd3a53146c416561aea6089
Md Taofick <107477685+taofick221@users.noreply.github.com>
2023-04-20 17:11:26 +0600

    Add files via upload

---
+++ Practice_Set.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Practice_Set {
+    public static void main(String[] args) {
+//Question-1---->
+//What will bo the result of the following expression float a= 7/4.0f*9/2.0f;
+//
+//        float a= 7/4.0f*9/2.0f;
+//        System.out.println(a);
+//Question-2:
+//        Write a java program to encrypt a grade by adding 8 to it. Decrypt it to show the correct grade.
+        char grade = 'B';
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+        // Decrypting the grade
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+//    /Question-3:
+//        Use comparison operators to find out whether a given number is greater than the user entered number or not.
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        System.out.println(a>8);
+//        System.out.println(7*49/7+35/7);
+//
+//
+
+
+
+    }
+}
